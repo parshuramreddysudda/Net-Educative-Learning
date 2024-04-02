b272056d5907fabeaf10edf8f0f4d7cee8919f50
Parshuram Reddy <parshuram.sudda@gmail.com>
2024-04-02 19:17:22 -0400

    Second Commit

--- Introdction-App/Program.cs
+++ Introdction-App/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace FirstProgram{
+
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello World");
+        }
+    }
+}
